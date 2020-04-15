4930e49dbe96ee4b8bcee764e118a86f50466468
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-04-15 17:20:42 -0500

    Clutter Cleanup
    Reorganized variables and reimplemented the vector operator example to match the singature unity implements

--- Assets/Chapter 1/Figures(Scripts)/Chapter1Fig3.cs
+++ Assets/Chapter 1/Figures(Scripts)/Chapter1Fig3.cs
@@ -1,81 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-// In this example, we will be subtracting Vector3's from one another
-//The following example demonstrates vector subtraction by taking the difference between two points—the mouse location and the center sphere
+// In this example, we will be subtracting Vector2's from one another
+// The following example demonstrates vector subtraction by taking the difference
+// between two points: the mouse location and the center sphere
 
 public class Chapter1Fig3 : MonoBehaviour
 {
-
-    //Create variables to mark the origin of our line
+    // These objects are brought in from the unity scene
+    public Camera camera;
+    public GameObject cursorSphere;
     public GameObject centerSphere;
-    private Vector3 centerSpherePosition;
 
-    //Create variables to track the mouse gameObject and position
-    private Vector3 mousePos;
-    public GameObject mouseCursor;
-
-    //Create variables for rendering the line between two vectors
-    private GameObject lineDrawing;
-    private LineRenderer lineRender;
-
-    //Float coordinates for our new vector3 we create via  "void subtractVector"
-    private float x, y, z;
-
-    //Our new vector3 we create via  "void subtractVector"
-    private Vector3 subtractedVector;
+    // A LineRenderer component will draw a line along our vector
+    private LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Instantiate a cursor GameObject to track the location of the mouse
+        // This is a clone of the 'mover' prefab
+        cursorSphere = Instantiate(cursorSphere, new Vector3(0, 0, 0), Quaternion.identity);
 
-        // Get the Vector3 (x,y,z) float coordinates of the center transform
-        centerSpherePosition = centerSphere.transform.position;
-
-        //Instantiate a cursor GameObject to track the location of the mouse
-        mouseCursor = Instantiate(mouseCursor, new Vector3(0, 0, 0), Quaternion.identity);
-
-        // Create a GameObject that will be the line
-        lineDrawing = new GameObject();
-
-        //Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a bright pink line.
-        lineRender = lineDrawing.AddComponent<LineRenderer>();
-
+        // Add the Unity Component "LineRenderer" to the GameObject this script is attached to
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Track the Vector3 of the mouse's position
-        mousePos = Input.mousePosition;
+        // Track the Vector2 of the mouse's position and the center sphere's position
+        Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 centerSpherePos = centerSphere.transform.position;
 
-        //Get the center of the transform
-        centerSpherePosition = centerSphere.transform.position;
+        // Subtract the two vectors 
+        Vector2 difference = subtractVectors(mousePos, centerSpherePos);
 
-        //Subtract the vector of the center from that of the mice position via "void subtractVector"
-        subtractVector(mousePos, centerSpherePosition);
-
-        //Begin rendering the line between the two objects. Set the first point (0) at the centerSphere Position
-        //Make sure the end of the line (1) appears at the new Vector3 we are creating via the "void subtractVector" 
-        lineRender.SetPosition(0, centerSpherePosition);
-        lineRender.SetPosition(1, new Vector3(x, y, z));
+        // Begin rendering the line between the two objects. Set the first point (0) at the centerSphere Position
+        // Make sure the end of the line (1) appears at the new Vector3 we are creating via the method "subtractVector" 
+        lineRenderer.SetPosition(0, centerSpherePos);
+        lineRenderer.SetPosition(1, difference);
 
         //Move the cursor to that same Vector3 we are creating via the "void subtractVector" 
-        mouseCursor.transform.position = subtractedVector;
-
+        cursorSphere.transform.position = mousePos;
     }
-
-    void subtractVector(Vector3 originalV3, Vector3 v)
+    
+    // This method calculates A - B component wise
+    // subtractVectors(vecA, vecB) will yield the same output as Unity's built in operator: vecA - vecB
+    Vector2 subtractVectors(Vector2 vectorA, Vector2 vectorB)
     {
-        // Dividing the subtraction by 100 to keep the cursor on the screen in this example
-        x = (originalV3.x - v.x) / 100;
-        y = (originalV3.y - v.y) / 100;
-        z = (originalV3.z - v.z) / 100;
-
-        subtractedVector = new Vector3(x, y, z);
+        float newX = vectorA.x - vectorB.x;
+        float newY = vectorA.y - vectorB.y;
+        return new Vector2(newX, newY);
     }
-
 }
-
-
