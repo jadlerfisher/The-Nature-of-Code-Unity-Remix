1ec61d78f2a9a79875067dfc9dfb75770ba672a4
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-04-16 16:50:06 -0500

    Refactored Example Code
    Added more steps and comments explaining the process of scaling a vector that is relative to another game object

--- Assets/Chapter 1/Figures(Scripts)/Chapter1Fig4.cs
+++ Assets/Chapter 1/Figures(Scripts)/Chapter1Fig4.cs
@@ -1,64 +1,58 @@
 ﻿using UnityEngine;
 
-// In this example, we will be subtracting Vector3's from one another
-//The following example demonstrates vector subtraction by taking the difference between two points—the mouse location and the center sphere
+// In this example, we will be scaling vector2's via multiplication.
+// The following example draws a vector from a central location to the mouse,
+// and either overshoots or undershoots the mouse based on the scale factor.
 
 public class Chapter1Fig4 : MonoBehaviour
 {
-
-    //Create variables to mark the origin of our line
+    // These objects are brought in from the unity scene
     public Camera camera;
     public GameObject centerSphere;
-    private Vector3 centerSpherePosition;
-
-    //Create variables to track the mouse gameObject and position
-    private Vector3 mousePos;
-    public GameObject mouseCursor;
+    public GameObject cursorSphere;
 
     //Create variables for rendering the line between two vectors
-    private GameObject lineDrawing;
     private LineRenderer lineRender;
-
 
     // Start is called before the first frame update
     void Start()
     {
-
-        // Get the Vector3 (x,y,z) float coordinates of the center transform
-        centerSpherePosition = centerSphere.transform.position;
-
-        //Instantiate a cursor GameObject to track the location of the mouse
-        mouseCursor = Instantiate(mouseCursor, new Vector3(0, 0, 0), Quaternion.identity);
-
-        // Create a GameObject that will be the line
-        lineDrawing = new GameObject();
-
-        //Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a bright pink line.
-        lineRender = lineDrawing.AddComponent<LineRenderer>();
-
+        // Add the Unity Component "LineRenderer" to the GameObject this script is attached to
+        lineRender = gameObject.AddComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Track the Vector3 of the mouse's position
+        // Track the Vector2 of the mouse's position and the center sphere's position
         Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 centerSpherePos = centerSphere.transform.position;
+        Vector2 centerPos = centerSphere.transform.position;
+        // Define a scaling coefficient to multiply x and y by
+        float scaleFactor = 0.5f;
 
-        //Get the center of the transform
-        centerSpherePosition = centerSphere.transform.position;
-
-        //Subtract the vector of the center from that of the mice position via "void subtractVector"
-        Vector2 sub = subtractVectors(mousePos,centerSpherePosition);
+        // Get a vector for the distance between the two spheres
+        Vector2 differenceVector = subtractVectors(mousePos, centerPos);
+        // Scale the vector
+        Vector2 scaledVector = multiplyVector(differenceVector, scaleFactor);
+        // Add the scaled vector back to the center position
+        Vector2 scaledMousePos = addVectors(centerPos, scaledVector);
         
-        //Begin rendering the line between the two objects. Set the first point (0) at the centerSphere Position
-        //Make sure the end of the line (1) appears at the new Vector3 we are creating via the "void subtractVector" 
-        lineRender.SetPosition(0, centerSpherePosition);
-        lineRender.SetPosition(1, sub);
+        // Begin rendering the line between the two objects. Set the first point (0) at the centerSphere position
+        // Make sure the end of the line (1) appears at the new Vector3 we are creating
+        lineRender.SetPosition(0, centerSphere.transform.position);
+        lineRender.SetPosition(1, scaledMousePos);
 
-        //Move the cursor to that same Vector3 we are creating via the "void subtractVector" 
-        mouseCursor.transform.position = sub;
+        // Move the cursor to that same Vector3 we created
+        cursorSphere.transform.position = scaledMousePos;
+    }
 
+    // This method calculates A + B component wise
+    // addVectors(vecA, vecB) will yield the same output as Unity's built in operator: vecA + vecB
+    Vector2 addVectors(Vector2 vectorA, Vector2 vectorB)
+    {
+        float newX = vectorA.x + vectorB.x;
+        float newY = vectorA.y + vectorB.y;
+        return new Vector2(newX, newY);
     }
 
     // This method calculates A - B component wise
