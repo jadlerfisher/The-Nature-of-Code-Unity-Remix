28e4bf69c86ff069cf3c1dafd8de28102779a77f
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-04-25 18:30:35 -0500

    Matched code updates from 2-3
    Code has been updated to match revisions made from feedback on 2-3.

--- Assets/Chapter 2/Figures(Scripts)/Chapter2Fig4.cs
+++ Assets/Chapter 2/Figures(Scripts)/Chapter2Fig4.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Chapter2Fig4 : MonoBehaviour
 {
+    // Geometry defined in the inspector.
+    public float floorY;
+    public float leftWallX;
+    public float rightWallX;
+    public Transform moverSpawnTransform;
+
     private List<Mover2_4> Movers = new List<Mover2_4>();
     // Define constant forces in our environment
     private Vector3 wind = new Vector3(0.002f, 0f, 0f);
-    private Vector3 gravity = Vector3.down * 9;
     private float frictionStrength = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 moverSpawnPosition = new Vector3(-7, 3, 0);
         // Create copys of our mover and add them to our list
         while (Movers.Count < 30)
         {
-            Movers.Add(new Mover2_4(moverSpawnPosition));
+            Movers.Add(new Mover2_4(
+                moverSpawnTransform.position,
+                leftWallX,
+                rightWallX,
+                floorY
+            ));
         }
     }
 
@@ -27,8 +36,6 @@ public class Chapter2Fig4 : MonoBehaviour
         // Apply the forces to each of the Movers
         foreach (Mover2_4 mover in Movers)
         {
-            // ForceMode.Acceleration ignores mass
-            mover.body.AddForce(gravity, ForceMode.Acceleration);
             // ForceMode.Impulse takes mass into account
             mover.body.AddForce(wind, ForceMode.Impulse);
 
@@ -49,18 +56,25 @@ public class Mover2_4
     private GameObject gameObject;
     private float radius;
 
-    public Mover2_4(Vector3 position)
+    private float xMin;
+    private float xMax;
+    private float yMin;
+
+    public Mover2_4(Vector3 position, float xMin, float xMax, float yMin)
     {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+
         // Create the components required for the mover
         gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         body = gameObject.AddComponent<Rigidbody>();
         // Remove functionality that come with the primitive that we don't want
         gameObject.GetComponent<SphereCollider>().enabled = false;
         Object.Destroy(gameObject.GetComponent<SphereCollider>());
-        body.useGravity = false;
 
         // Generate random properties for this mover
-        radius = Random.Range(0.2f, 0.6f);
+        radius = Random.Range(0.1f, 0.4f);
 
         // Place our mover at the specified spawn position relative
         // to the bottom of the sphere
@@ -80,15 +94,21 @@ public class Mover2_4
     public void CheckBoundaries()
     {
         Vector3 restrainedVelocity = body.velocity;
-        if (body.position.y - radius < -4.5f)
+        if (body.position.y - radius < yMin)
         {
+            // Using the absolute value here is and important safe
+            // guard for the scenario that it takes multiple ticks
+            // of FixedUpdate for the mover to return to its boundaries.
+            // The intuitive solution of flipping the velocity may result
+            // in the mover not returning to the boundaries and flipping
+            // direction on every tick.
             restrainedVelocity.y = Mathf.Abs(restrainedVelocity.y);
         }
-        if (body.position.x - radius < -8f)
+        if (body.position.x - radius < xMin)
         {
             restrainedVelocity.x = Mathf.Abs(restrainedVelocity.x);
         }
-        else if (body.position.x + radius > 8f)
+        else if (body.position.x + radius > xMax)
         {
             restrainedVelocity.x = -Mathf.Abs(restrainedVelocity.x);
         }
