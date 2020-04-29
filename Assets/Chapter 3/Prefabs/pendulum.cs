ddd3f9ff115035f5d537aefc585efd7563fd2665
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-04-29 17:13:15 -0500

    Refactored pendulum example
    Exposed more properties to the inspector, fixed functionality with deltaTime, seperated more functionality into the pendulum class

--- Assets/Chapter 3/Prefabs/pendulum.cs
+++ Assets/Chapter 3/Prefabs/pendulum.cs
@@ -2,48 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pendulum : MonoBehaviour
+public class Pendulum : MonoBehaviour
 {
-
-    Vector3 location;
-    Vector3 origin = new Vector3(0f,10f,0f);
-
-    public float r;
-    public float angle;
-    public float aVelocity =0.0f;
-    public float aAcceleration = 0.0f;
-    public float damping = 0.995f;
-    GameObject lineDrawing;
-    LineRenderer lineRender;
+    // Public variables can be modified by other scripts
+    public Vector2 pivot = Vector2.zero;
+    public float radius = 1;
+    public float angle = 0;
+    public float damping = 1;
+    public float gravity = 0;
+    // Other scripts cannot see private variables
+    LineRenderer lineRenderer;
+    Transform bobTransform;
+    float aVelocity;
+    float aAcceleration;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Create a GameObject that will be the line
-        lineDrawing = new GameObject();
+        // Initialize the pendulum in a resting state
+        aVelocity = 0;
+        aAcceleration = 0;
 
-        //Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a bright pink line.
-        lineRender = lineDrawing.AddComponent<LineRenderer>();
+        // Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a bright pink line.
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        // Create a sphere for the bob of the pendulum
+        GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        newObject.GetComponent<SphereCollider>().enabled = false;
+        Destroy(newObject.GetComponent<SphereCollider>());
+        bobTransform = newObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void Go() {
-
-        float gravity = .4f;
-        aAcceleration = -1 * gravity * Mathf.Sin(angle);
-        aVelocity += aAcceleration;
+        // Update the force being applied on this pendulum
+        aAcceleration = -gravity * Mathf.Sin(angle);
+        aVelocity += aAcceleration * Time.deltaTime;
         angle += aVelocity;
+        // Apply damping to slow down the velocity over time
+        aVelocity *= damping;
 
-        this.gameObject.transform.position = new Vector3(r * Mathf.Sin(angle), r * Mathf.Cos(angle), 0f);
-        this.gameObject.transform.position += origin;
-
-
-        lineRender.SetPosition(0, origin);
-        lineRender.SetPosition(1, this.gameObject.transform.position);
+        // Reposition the bob by converting from polar coordinates
+        bobTransform.position = pivot + new Vector2(
+            radius * Mathf.Sin(angle),
+            radius * Mathf.Cos(angle)
+        );
+        lineRenderer.SetPosition(0, pivot);
+        lineRenderer.SetPosition(1, bobTransform.position);
     }
 }
