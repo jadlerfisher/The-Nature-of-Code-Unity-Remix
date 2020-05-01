4ab2ca6e9b436c1ce61a4351c4071b07d5220b47
sim2kid <sim2kid@users.noreply.github.com>
2020-04-30 22:45:34 -0500

    Removed V3s and replaced with V2. Also removed and resized the scene's objects

--- Assets/Chapter 3/Figures(Scripts)/Chapter3Fig5.cs
+++ Assets/Chapter 3/Figures(Scripts)/Chapter3Fig5.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Chapter3Fig5 : MonoBehaviour
 {
-    float period = 22f;
-    float amplitude = 20f;
+    public float period = 5f;
+    public float amplitude = 5f;
 
-    public GameObject sphere;
+    private GameObject sphere;
 
     //Create variables for rendering the line between two vectors
     private GameObject lineDrawing;
@@ -16,26 +16,30 @@ public class Chapter3Fig5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 5;
         // Create a GameObject that will be the line
         lineDrawing = new GameObject();
 
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
         //Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a bright pink line.
         lineRender = lineDrawing.AddComponent<LineRenderer>();
+        lineRender.GetComponent<LineRenderer>().startWidth = 0.1f;
+        lineRender.GetComponent<LineRenderer>().endWidth = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         float x = amplitude * Mathf.Cos((2*Mathf.PI)* Time.time/period);
 
-        sphere.transform.position = new Vector3(x, 0f, 0f);
+        sphere.transform.position = new Vector2(x, 0f);
 
 
         //Begin rendering the line between the two objects. Set the first point (0) at the centerSphere Position
         //Make sure the end of the line (1) appears at the new Vector3
-        Vector3 center = new Vector3(0f, 0f, 0f);
+        Vector2 center = Vector2.zero;
         lineRender.SetPosition(0, center);
         lineRender.SetPosition(1, sphere.transform.position);
 
