3aa36d555a39420dfcac6087d6fb890775d88566
jadlerfisher <jadlerfisher@gmail.com>
2020-05-02 00:14:20 -0500

    Revert "Merge remote-tracking branch 'origin/2-6_revision'"
    This reverts commit c8fa9fa088c0daabd151b373bd0d3f0a2e2ed155, reversing
    changes made to 83660f4f85245cd01c7b0d1af665424cd56b277c.

--- Assets/Chapter 2/Figures(Scripts)/Chapter2Fig6.cs
+++ Assets/Chapter 2/Figures(Scripts)/Chapter2Fig6.cs
@@ -4,125 +4,33 @@ using UnityEngine;
 
 public class Chapter2Fig6 : MonoBehaviour
 {
-    Mover m; // A Mover and an Attractor
-    Attractor a;
+
+    public GameObject a;
+    public GameObject m;
+    Vector3 force;
+
+    private attractorChapter2_6 aC26;
+    private moverChapter2_6 mC26;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        m = new Mover();
-        a = new Attractor();
+        a = Instantiate(a);
+        m = Instantiate(m, new Vector3(4f, 0f, 0f), Quaternion.identity);
+
+        aC26 = a.GetComponent<attractorChapter2_6>();
+        mC26 = m.GetComponent<moverChapter2_6>();
+ 
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 force = a.Attract(m); // Apply the attraction from the Attractor on the Mover
-        m.ApplyForce(force);
-        m.Update();
-        a.Update();
-    }
-}
+        force = aC26.attract(mC26);
+        mC26.applyForce( new Vector3 (1f, 0f, 0f));
+        mC26.applyForce(force);
 
-
-public class Attractor
-{
-    public float mass;
-    private Vector2 location;
-    private float G;
-
-    private GameObject attractor;
-
-    public Attractor()
-    {
-        attractor = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        Renderer renderer = attractor.GetComponent<Renderer>();
-        renderer.material = new Material(Shader.Find("Diffuse"));
-        renderer.material.color = Color.red;
-
-        location = Vector2.zero;
-        mass = 20f;
-        G = 9.8f;
-    }
-
-    public Vector2 Attract(Mover m)
-    {
-        Vector2 force = location - m.location;
-        float distance = force.magnitude;
-
-        // Remember we need to constrain the distance so that our circle doesn't spin out of control
-        distance = Mathf.Clamp(distance, 5f, 25f);
-
-        force.Normalize();
-        float strength = (G * mass * m.mass) / (distance * distance);
-        force *= strength;
-        return force;
-    }
-
-    public void Update()
-    {
-        attractor.transform.position = location;
-    }
-}
-
-public class Mover
-{
-    // The basic properties of a mover class
-    public Vector2 location, velocity, acceleration;
-    public float mass;
-
-    private Vector2 minimumPos, maximumPos;
-
-    private GameObject mover;
-
-    public Mover()
-    {
-        mover = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        Renderer renderer = mover.GetComponent<Renderer>();
-        renderer.material = new Material(Shader.Find("Diffuse"));
-        renderer.material.color = Color.white;
-
-        mass = 1;
-        location = new Vector2(5, 0); // Default location
-        velocity = new Vector2(0, -4); // The extra velocity makes the mover orbit
-        acceleration = Vector2.zero;
-        findWindowLimits();
-    }
-
-    public void ApplyForce(Vector2 force)
-    {
-        Vector2 f = force / mass;
-        acceleration += f;
-    }
-
-    public void Update()
-    {
-        velocity += acceleration * Time.deltaTime;
-        location += velocity * Time.deltaTime;
-
-        acceleration = Vector2.zero;
-
-        mover.transform.position = location;
-        CheckEdges();
-    }
-
-    public void CheckEdges()
-    {
-        if (location.x > maximumPos.x || location.x < minimumPos.x)
-        {
-            velocity.x *= -1;
-        }
-        if (location.y > maximumPos.y || location.y < minimumPos.y)
-        {
-            velocity.y *= -1;
-        }
-    }
-
-    private void findWindowLimits()
-    {
-        Camera.main.orthographic = true;
-        Camera.main.orthographicSize = 8;
-        minimumPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
-        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 }
---
+++ Assets/Chapter 2/Figures(Scripts)/Chapter2Fig6RB.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter2Fig6RB : MonoBehaviour
+{
+
+    public GameObject a;
+    public GameObject m;
+    Vector3 force;
+
+    private attractorChapter2_6 aC26;
+    private moverChapter2_6 mC26;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+ 
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 difference = this.transform.position - m.transform.position;
+        float dist = difference.magnitude;
+        Vector3 gravityDirection = difference.normalized;
+        float gravity = 6.7f * (this.transform.localScale.x * m.transform.localScale.x * 80) / (dist * dist);
+
+        Vector3 gravityVector = (gravityDirection * gravity);
+        m.transform.GetComponent<Rigidbody>().AddForce(m.transform.forward, ForceMode.Acceleration);
+        m.transform.GetComponent<Rigidbody>().AddForce(gravityVector, ForceMode.Acceleration);
+    }
+}
