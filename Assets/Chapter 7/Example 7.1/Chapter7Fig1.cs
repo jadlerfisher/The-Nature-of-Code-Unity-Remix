bb04bf7e81f9ab4b49c1fd4951e3bd3b2ee5939c
Chris Grych <ceggrych@gmail.com>
2020-06-13 14:44:20 -0500

    Adds comments, finishes up 7.1

--- Assets/Chapter 7/Example 7.1/Chapter7Fig1.cs
+++ Assets/Chapter 7/Example 7.1/Chapter7Fig1.cs
@@ -1,40 +1,119 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Chapter7Fig1 : MonoBehaviour
+/// <summary>
+/// Chapter 7 Figure 1: Wolfram Elementary Cellular Automata
+/// </summary>
+
+public class Chapter7Fig1 : MonoBehaviour 
 {
+    // A list to store ruleset arrays
+    public List<int[]> rulesetList = new List<int[]>();
+    
+    // Custom Rulesets
+    public int[] ruleSet0 = { 0, 1, 0, 1, 1, 0, 1, 0 };
+    public int[] ruleSet1 = { 1, 0, 1, 0, 1, 0, 1, 0 };
+    public int[] ruleSet2 = { 0, 1, 0, 1, 1, 0, 1, 0 };
+    public int[] ruleSet3 = { 1, 1, 0, 0, 1, 0, 1, 1 };
+    public int[] ruleSet4 = { 0, 0, 0, 1, 1, 0, 1, 0 };
+    public int[] ruleSet5 = { 1, 0, 0, 1, 1, 0, 1, 0 };
+    public int[] ruleSet6 = { 0, 1, 1, 0, 1, 0, 1, 1 };
+    
+    private int rulesChosen;
+
+    // An object to describe a Wolfram elementary Cellular Automata
     Chapter7Fig1CA ca;
+
+    // How long after the CA has been drawn before reloading the scene, choosing new rule
+    private int delay = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        int[] ruleset = { 0, 1, 0, 1, 1, 0, 1, 0 };
-        ca = new Chapter7Fig1CA(ruleset);        
+        addRuleSetsToList();
+
+        // Choosing a random rule set using Random.Range
+        rulesChosen = Random.Range(0, rulesetList.Count);
+        int[] ruleset = rulesetList[rulesChosen]; 
+        ca = new Chapter7Fig1CA(ruleset); // Initialize CA
+
+        limitFrameRate();
+        setOrthographicCamera();
+    }    
+
+    private void Update() 
+    {
+        ca.Display(); // Draw the CA
+        ca.Generate();
+
+        if (ca.IsFinished()) // If we're done, clear the screen, pick a new ruleset and restart
+        {
+            delay++;
+            if (delay > 30) 
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload current scene                
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload current scene            
+        }           
     }
 
-    private void Update()
+    private void addRuleSetsToList()
     {
-        ca.Display();
-        ca.Generate();
+        rulesetList.Add(ruleSet0);
+        rulesetList.Add(ruleSet1);
+        rulesetList.Add(ruleSet2);
+        rulesetList.Add(ruleSet3);
+        rulesetList.Add(ruleSet4);
+        rulesetList.Add(ruleSet5);
+        rulesetList.Add(ruleSet6);
+    }
+
+    private static void setOrthographicCamera()
+    {
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 10;
+    }
+
+    private void limitFrameRate()
+    {
+        Application.targetFrameRate = 30; 
+        QualitySettings.vSyncCount = 0;
     }
 }
 
 public class Chapter7Fig1CA
 {
-    private int[] cells;
-    private int generation;
-    private int[] ruleset; // TODO make these exposed
-    private int w = 5;
+    private int[] cells; // An array of 0s and 1s
+    private int generation; // How many generations?
+    private int[] ruleset; // An array to store the ruleset, for example {0,1,1,0,1,1,0,1}
+    private int rowWidth; // How wide to make the array
+    private int cellCapacity; // We limit how many cells we instantiate
+    private int numberOfCells; // Which needs us to keep count
+    private Vector2 screenSize; 
+    private float yScreenOffset; // Cells are spawned with a small offset so they spawn in centered
+    private float xScreenOffset; 
 
-    public Chapter7Fig1CA(int[] r)
+    public Chapter7Fig1CA(int[] ruleSetToUse)
     {
-        ruleset = r;
-        cells = new int[(int)Camera.main.orthographicSize * 2];
+        rowWidth = 17;
+        cellCapacity = 650;
+        yScreenOffset = 2.5f;
+        xScreenOffset = 1.25f;
+
+        // How big our screen is in World Units
+        screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));        
+        numberOfCells = 0;
+        ruleset = ruleSetToUse;
+        cells = new int[cellCapacity / rowWidth];
         restart();
     }
 
-    public void Randomize()
+    public void Randomize() // If we wanted to make a random Ruleset
     {
         for (int i = 0; i < 8; i++)
         {
@@ -48,47 +127,59 @@ public class Chapter7Fig1CA
         {
             cells[i] = 0;
         }
-        cells[cells.Length / 2] = 1;
+        cells[cells.Length / 2] = 1; // We arbitrarily start with just the middle cell having a state of "1"
         generation = 0;
     }
 
+    // The process of creating the new generation
     public void Generate()
     {
+        // First we create an empty array for the new values
         int[] nextGen = new int[cells.Length];
+
+        // For every spot, determine new state by examing current state, and neighbor states
+        // Ignore edges that only have one neighor
         for (int i = 1; i < cells.Length-1; i++)
         {
-            int left = cells[i - 1];
-            int me = cells[i];
-            int right = cells[i + 1];
-            nextGen[i] = rules(left, me, right);
+            int left = cells[i - 1]; // Left neighbor state
+            int me = cells[i]; // Current state
+            int right = cells[i + 1]; // Right neighbor state
+            nextGen[i] = rules(left, me, right); // Compute next generation state based on ruleset
         }
 
+        // The current generation is the new generation
         cells = nextGen;
         generation++;
     }
-
-    public void Display()
+    
+    public void Display() // Drawing the cells. Cells with a state of 1 are black, cells with a state of 0 are white
     {
-        for (int i = 0; i < cells.Length; i++)
+     if (numberOfCells <= cellCapacity)
         {
-            GameObject newCell = GameObject.CreatePrimitive(PrimitiveType.Cube); // TODO Plane? Quad?
-            newCell.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-            Renderer r = newCell.GetComponent<Renderer>();
-            r.material = new Material(Shader.Find("Diffuse"));
-            Object.Destroy(newCell.GetComponent<BoxCollider>());
-            if (cells[i] == 1)
+            for (int i = 0; i < cells.Length; i++)
             {
-                r.material.color = Color.black;
+                GameObject newCell = GameObject.CreatePrimitive(PrimitiveType.Quad);                
+                numberOfCells++;                
+                Renderer r = newCell.GetComponent<Renderer>();
+                r.material = new Material(Shader.Find("Diffuse"));
+                Object.Destroy(newCell.GetComponent<BoxCollider>());
+                if (cells[i] == 1)
+                {
+                    r.material.color = Color.black;
+                }
+                else
+                {
+                    r.material.color = Color.white;
+                }
+
+                // Set position based to lower left of screen, with screen offset
+                newCell.transform.position = new Vector3(i - screenSize.x - xScreenOffset, 
+                                                generation - screenSize.y + yScreenOffset);
             }
-            else
-            {
-                r.material.color = Color.white;
-            }
-            newCell.transform.position = new Vector3((i - Camera.main.orthographicSize) * 0.5f, generation * 0.5f, 100f);
         }
     }
 
-    private int rules (int a, int b, int c)
+    private int rules (int a, int b, int c) // Implementing the Wolfram rules
     {
         if (a == 1 && b == 1 && c == 1) return ruleset[0];
         if (a == 1 && b == 1 && c == 0) return ruleset[1];
@@ -101,9 +192,9 @@ public class Chapter7Fig1CA
         return 0;
     }
 
-    public bool IsFinished()
+    public bool IsFinished() // The CA is done if it reaches a height limit
     {
-        if (generation > Screen.height / w)
+        if (generation > Screen.height / rowWidth)
             return true;
         else
             return false;
