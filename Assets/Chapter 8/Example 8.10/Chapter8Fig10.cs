b12e131d50d3da958cd112a7abe90aafb957890c
Chris Grych <ceggrych@gmail.com>
2020-06-17 16:41:39 -0500

    Starts l-system

---
+++ Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Example 6.10 L-System
+/// </summary>
+
+public class Chapter8Fig10 : MonoBehaviour
+{
+    private Chapter8Fig10LSystem lSys;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Chapter8Fig10LSystem
+{
+    public string Sentence { get; private set; }
+    private Chapter8Fig10Rule[] ruleset;
+    public int Generation { get; private set; }
+
+    public Chapter8Fig10LSystem(string axiom, Chapter8Fig10Rule[] r)
+    {
+        Sentence = axiom;
+        ruleset = r;
+        Generation = 0;
+    }
+
+    private void Generate()
+    {
+        char[] sentenceCharArray = Sentence.ToCharArray();
+        System.Text.StringBuilder nextGen = new StringBuilder();
+        for (int i = 0; i < Sentence.Length; i++) 
+        {
+            char curr = sentenceCharArray[i];
+            string replace = "" + curr;
+            for (int j = 0; j < ruleset.Length; j++)
+            {
+                char a = ruleset[j].A;
+                if (a == curr)
+                {
+                    replace = ruleset[j].B;
+                    break;
+                }
+            }
+            nextGen.Append(replace);
+        }
+        Sentence = nextGen.ToString();
+        Generation++;
+    }
+    
+}
+
+public class Chapter8Fig10Rule
+{
+    public char A { get; private set; }
+    public string B { get; private set; }
+
+    public Chapter8Fig10Rule(char _a, string _b)
+    {
+        A = _a;
+        B = _b;
+    }
+}
+
+public class Chapter8Fig10Turtle
+{
+    private string todo;
+    private float len;
+    private float theta;
+
+
+}
