158dae780da68af9122925301fb9cf11f825e27c
Chris Grych <ceggrych@gmail.com>
2020-06-20 13:43:59 -0500

    Adds saved transforms

--- Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
+++ Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
@@ -13,36 +13,33 @@ public class Chapter8Fig10 : MonoBehaviour
     private Chapter8Fig10Turtle turtle;
 
     private Vector2 screenSize;
-    private Transform lastState;
+    private Vector3 savedPos;
+    private Quaternion savedRotate;
 
     private int counter;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        g.transform.position = Vector3.zero;
-        g.transform.position += new Vector3(10, 10, 10);
         // P changing the transofmr only changes any object drawn AFTER the transformation, and resets to normal coordinates on the very next draw!
+        // Modifying transform will change all objects even fi they're already drawn!
 
-        lastState = transform;
+        
         counter = 0;
         screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
         Chapter8Fig10Rule[] ruleset = new Chapter8Fig10Rule[1];
         ruleset[0] = new Chapter8Fig10Rule('F', "FF+[+F-F-F]-[-F+F+F]");
         lSys = new Chapter8Fig10LSystem("F", ruleset);
-        //turtle = new Chapter8Fig10Turtle(lSys.Sentence, screenSize.y / 3, 25 * Mathf.Deg2Rad); 
+        turtle = new Chapter8Fig10Turtle(lSys.Sentence, screenSize.y / 3, 25 * Mathf.Deg2Rad); 
         redraw();
     }
 
     private void redraw()
     {
-        transform.position = Vector3.zero;
-        transform.rotation = Quaternion.Euler(Vector3.zero);
+        
         transform.position = new Vector2(screenSize.x / 2, screenSize.y);
         transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z - 90);
-        // turtle.Render();
+        turtle.Render();
     }
 
     // Update is called once per frame
@@ -52,11 +49,12 @@ public class Chapter8Fig10 : MonoBehaviour
         {
             if (counter < 5)
             {
-                lastState.position = transform.position;
-                lastState.rotation = transform.rotation;
+                // P
+                //savedPos = transform.position;
+                
                 lSys.Generate();
-                // turtle.Sentence = lSys.Sentence;
-                // turtle.Len *= 0.5f;
+                turtle.Todo= lSys.Sentence;
+                turtle.Len *= 0.5f;
                 redraw();
                 counter++;
             }
@@ -70,35 +68,52 @@ public class Chapter8Fig10Turtle
     public float Len;
     private float theta;
 
-    private Transform currentTransform;
-    private Transform savedTransform;
-
-    public Chapter8Fig10Turtle(Transform current, Transform saved, string s, float l, float t)
+    public Chapter8Fig10Turtle(string s, float l, float t)
     {
         Todo = s;
         Len = l;
         theta = t;
-        currentTransform = current;
-        savedTransform = saved;
     }
 
     public void Render()
     {
+        Vector2 newX = Vector2.zero;
+        Vector3 newRotate = Vector3.zero;
+        Vector2 oldX = Vector2.zero;
+        Vector3 oldRotate = Vector3.zero;
         for (int i = 0; i < Todo.Length; i++)
         {
             char[] todoCharArray = Todo.ToCharArray();
             char c = todoCharArray[i];
+            
             if (c == 'F' || c == 'G')
             {
                 GameObject g = new GameObject();
-                g.transform.position = currentTransform.position;
-                g.transform.rotation = currentTransform.rotation;
+                g.transform.rotation = Quaternion.Euler(newRotate);
                 LineRenderer l = g.AddComponent<LineRenderer>();
                 l.useWorldSpace = false;
                 l.positionCount = 2;
-                l.SetPosition(0, Vector2.zero);
-                l.SetPosition(1, new Vector2(Len, 0));
-                currentTransform.position += new Vector3(Len, 0);
+                l.SetPosition(0, Vector2.zero + newX);
+                l.SetPosition(1, new Vector2(Len + newX.x, 0 + newX.y));
+                newX += new Vector2(Len, 0);
+            }
+            else if (c == '+')
+            {
+                newRotate.z += theta;
+            }
+            else if (c == '-')
+            {
+                newRotate.z -= theta;
+            }
+            else if (c == '[')
+            {
+                oldX = newX;
+                oldRotate = newRotate;
+            }
+            else if (c == ']')
+            {
+                newX = oldX;
+                newRotate = oldRotate;
             }
         }
     }
