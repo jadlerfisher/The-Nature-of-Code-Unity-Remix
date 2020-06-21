ded7648c9817f87a42f9b526db7be0645cd35623
Thomas Rohn <trohn54@gmail.com>
2020-06-21 16:01:22 -0500

    finish 8.2 and 8.3

--- Assets/Chapter 8/Example 8.1/Chapter8Fig1.cs
+++ Assets/Chapter 8/Example 8.1/Chapter8Fig1.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Chapter8Fig1 : MonoBehaviour
 {
+    [SerializeField] float initialRadius;
+    [SerializeField] float minimumRadius;
+    [SerializeField] float strokesize;
+    [SerializeField] float reductionRate;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var circle = new GameObject { name = "Circle" };        
+        RecursiveCircle(initialRadius);
     }
 
-    // Update is called once per frame
-    void Update()
+    void RecursiveCircle(float radius)
     {
-        
+        var circle = new GameObject();
+        circle.DrawCircle(radius, strokesize);
+        if (radius > minimumRadius)
+        {
+            radius *= reductionRate;
+            RecursiveCircle(radius);
+        }
     }
 }
---
+++ Assets/Chapter 8/Example 8.3/Chapter8Fig3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter8Fig3 : MonoBehaviour
+{
+    [SerializeField] float initialRadius;
+    [SerializeField] float minimumRadius;
+    [SerializeField] float strokesize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var circle = new GameObject { name = "Circle" };
+        RecursiveCircle(initialRadius, 0, 0);
+    }
+
+    void RecursiveCircle(float radius, float xPos, float zPos)
+    {
+        var circle = new GameObject();
+        circle.DrawCircle(radius, strokesize, xPos, zPos);
+
+        //Very important to make sure
+        if (radius > minimumRadius)
+        {            
+            RecursiveCircle(radius / 2, xPos + radius, zPos);
+            RecursiveCircle(radius / 2, xPos - radius, zPos);
+            RecursiveCircle(radius / 2, xPos, zPos + radius);
+            RecursiveCircle(radius / 2, xPos, zPos - radius);
+
+        }
+    }
+}
