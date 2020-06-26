6fe9d88bd7e6435da6dfda6a51ae5412afacb20e
Chris Grych <ceggrych@gmail.com>
2020-06-25 22:31:38 -0500

    Made some changes

--- Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
+++ Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
@@ -7,43 +7,109 @@ using UnityEngine;
 /// Example 6.10 L-System
 /// </summary>
 
+public struct SavedTransform
+{
+    public Vector3 Position;
+    public Quaternion Rotation;
+}
+
 public class Chapter8Fig10 : MonoBehaviour
 {
     private Chapter8Fig10LSystem lSys;
-    private Chapter8Fig10Turtle turtle;
-
     private Vector2 screenSize;
+    private string todo;
+    private float len;
+    private float theta;
+    private Stack<SavedTransform> savedTransforms;
+    private SavedTransform currentTransform;
 
     private int counter;
-
-    private Stack<Vector3> savedPositions;
-    private Stack<Quaternion> savedRotations;
 
     // Start is called before the first frame update
     void Start()
     {
-        // In P, changing the coordinate matrix only affects objects that are drawn after the manipulation, not before. So we can't child objects to this GO and change the transform after the fact as the child objects will match the new Transform.
-
-        // In P, any manipulation done to the coordinate matrices get reset at the start of every draw.
-        
+        savedTransforms = new Stack<SavedTransform>();
+        currentTransform.Position = transform.position;
+        currentTransform.Rotation = transform.rotation;
         counter = 0;
-        screenSize = Camera.main.ScreenToWorldPoint(new Vector2(10f, 10f));
+        screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
         
         Chapter8Fig10Rule[] ruleset = new Chapter8Fig10Rule[1];
         ruleset[0] = new Chapter8Fig10Rule('F', "FF+[+F-F-F]-[-F+F+F]");
         lSys = new Chapter8Fig10LSystem("F", ruleset);
-        savedPositions = new Stack<Vector3>();
-        savedRotations = new Stack<Quaternion>();
-        
-        turtle = new Chapter8Fig10Turtle(lSys.Sentence, screenSize.y / 3, 25, savedPositions, savedRotations); 
+
+        todo = lSys.Sentence;
+        len = screenSize.y / 3;
+        theta = Mathf.Rad2Deg * (-Mathf.PI / 2);
+
         redraw();
+    }
+
+    private void saveTransform()
+    {
+        currentTransform.Position = transform.position;
+        currentTransform.Rotation = transform.rotation;
+    }
+
+    private void pushTransformStack()
+    {
+        savedTransforms.Push(currentTransform);
+    }
+
+    private SavedTransform popTransformStack()
+    {
+        return savedTransforms.Pop();
     }
 
     private void redraw()
     {
-        transform.position = new Vector2(0f, -screenSize.y);
+        transform.position = new Vector2(0f, -screenSize.y + len);
+        transform.rotation = Quaternion.Euler(Vector3.zero);
         transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + Mathf.Rad2Deg * (-Mathf.PI / 2));
-        turtle.Render();
+        saveTransform();
+        render();
+    }
+
+    private void render()
+    {
+        for (int i = 0; i < todo.Length; i++)
+        {
+            char[] todoCharArray = todo.ToCharArray();
+            char c = todoCharArray[i];
+            if (c == 'F' || c == 'G')
+            {
+                GameObject g = new GameObject();
+                g.transform.position = currentTransform.Position;
+                g.transform.rotation = currentTransform.Rotation;
+                LineRenderer line = g.AddComponent<LineRenderer>();
+                line.positionCount = 2;
+                line.useWorldSpace = true;
+                line.SetPosition(0, Vector2.zero);
+                line.SetPosition(1, new Vector2(len, 0));
+                currentTransform.Position += new Vector3(len, 0);
+                line.startWidth = 0.1f;
+                line.endWidth = 0.1f;
+                
+            }
+            else if (c == '+')
+            {
+                currentTransform.Rotation = Quaternion.Euler(currentTransform.Rotation.x, currentTransform.Rotation.y, currentTransform.Rotation.z + theta);
+                
+            }
+            else if (c == '-')
+            {
+                currentTransform.Rotation = Quaternion.Euler(currentTransform.Rotation.x, currentTransform.Rotation.y, currentTransform.Rotation.z - theta);
+                
+            }
+            else if (c == '[')
+            {
+                pushTransformStack();
+            }
+            else if (c == ']')
+            {
+                currentTransform = popTransformStack();
+            }
+        }
     }
 
     // Update is called once per frame
@@ -53,67 +119,13 @@ public class Chapter8Fig10 : MonoBehaviour
         {
             if (counter < 5)
             {
-                savedPositions.Push(transform.position);
-                savedRotations.Push(transform.rotation);
+                pushTransformStack();
                 lSys.Generate();
-                turtle.Todo= lSys.Sentence;
-                turtle.Len *= 0.5f;
-                transform.position = savedPositions.Pop();
-                transform.rotation = savedRotations.Pop();
+                todo = lSys.Sentence;
+                len *= 0.5f;
+                currentTransform = popTransformStack();
                 redraw();
                 counter++;
-            }
-        }
-    }
-}
-
-public class Chapter8Fig10Turtle
-{
-    public string Todo;
-    public float Len;
-    private float theta;
-    private Stack<Vector3> savedPositions;
-    private Stack<Quaternion> savedRotations;
-
-    // Transforms are reference-based so if we make a new var and assign it to the GO's Transform, if we manipulate the new var's values, the GO's transform will change along with it. So we use Vector's and Quaternions as they are structs 
-
-    public Chapter8Fig10Turtle(string s, float l, float t, Stack<Vector3> savedPos, Stack<Quaternion> savedRot)
-    {
-        Todo = s;
-        Len = l;
-        theta = t;
-        savedPositions = savedPos;
-        savedRotations = savedRot;
-    }
-
-    public void Render()
-    {
-        for (int i = 0; i < Todo.Length; i++)
-        {
-            char[] todoCharArray = Todo.ToCharArray();
-            char c = todoCharArray[i];
-            
-            if (c == 'F' || c == 'G')
-            {
-                GameObject g = new GameObject();
-                LineRenderer r = g.AddComponent<LineRenderer>();
-                // Make line points
-            }
-            else if (c == '+')
-            {
-                
-            }
-            else if (c == '-')
-            {
-                
-            }
-            else if (c == '[')
-            {
-                
-            }
-            else if (c == ']')
-            {
-                
             }
         }
     }
