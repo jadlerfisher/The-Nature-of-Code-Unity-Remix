e7ab99634dfdedf5e0e046e5dd0c164720b8d897
Chris Grych <ceggrych@gmail.com>
2020-06-27 20:53:34 -0500

    Adds l system tree

--- Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
+++ Assets/Chapter 8/Example 8.10/Chapter8Fig10.cs
@@ -5,111 +5,140 @@ using UnityEngine;
 
 /// <summary>
 /// Example 6.10 L-System
-/// </summary>
+/// Unity L-System Generation by Dilmer Valecillos
+/// https://github.com/dilmerv/UnityLSystemEssentials
+/// </summary> 
 
-public struct SavedTransform
-{
-    public Vector3 Position;
-    public Quaternion Rotation;
-}
-
+// By using the RequireComponent attribute, this script requires that the specified component must be on the GameObject
+// It is attached to, and disallows removing the specified component from the GameObject while this script is attached
+[RequireComponent(typeof(LineRenderer))]
 public class Chapter8Fig10 : MonoBehaviour
 {
+    public Material branchMaterial;
+
     private Chapter8Fig10LSystem lSys;
     private Vector2 screenSize;
-    private string todo;
     private float len;
     private float theta;
-    private Stack<SavedTransform> savedTransforms;
-    private SavedTransform currentTransform;
+    private LineRenderer lineRenderer;
+    private Chapter8Fig10LSystemState state;
+
+    // A stack is a collection of objects that functions like a list in that the size is dynamic. You can do one of two things with a stack: 
+    // Place an object on top of the stack (Push) Or take the object off the top of the stack and assign/manipulate it. 
+    // Think of it like a stack of plates. You can either place a plate on top of the stack or take one from the top.
+    private Stack<Chapter8Fig10LSystemState> savedStates;
+    private List<GameObject> lines;
 
     private int counter;
 
     // Start is called before the first frame update
     void Start()
     {
-        savedTransforms = new Stack<SavedTransform>();
-        currentTransform.Position = transform.position;
-        currentTransform.Rotation = transform.rotation;
+        lines = new List<GameObject>();
+        savedStates = new Stack<Chapter8Fig10LSystemState>();
+        state = new Chapter8Fig10LSystemState();
         counter = 0;
-        screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        setupCamera();        
+
+        // Start drawing lines by modifying the GO's location to be bottom center
+        transform.position = new Vector3(0f, -screenSize.y);
         
         Chapter8Fig10Rule[] ruleset = new Chapter8Fig10Rule[1];
         ruleset[0] = new Chapter8Fig10Rule('F', "FF+[+F-F-F]-[-F+F+F]");
         lSys = new Chapter8Fig10LSystem("F", ruleset);
 
-        todo = lSys.Sentence;
-        len = screenSize.y / 3;
-        theta = Mathf.Rad2Deg * (-Mathf.PI / 2);
+        len = screenSize.y / 6;
 
-        redraw();
+        // Degress of rotation
+        theta = 45;
+
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.material = branchMaterial;
+
+        // Set both the start and end width to 0.05
+        lineRenderer.startWidth = lineRenderer.endWidth = 0.05f;
+        savedStates.Push(state.Clone());
+        lSys.Generate();
+        state = savedStates.Pop();
+        drawLines();
     }
 
-    private void saveTransform()
+    private void drawLines()
     {
-        currentTransform.Position = transform.position;
-        currentTransform.Rotation = transform.rotation;
-    }
-
-    private void pushTransformStack()
-    {
-        savedTransforms.Push(currentTransform);
-    }
-
-    private SavedTransform popTransformStack()
-    {
-        return savedTransforms.Pop();
-    }
-
-    private void redraw()
-    {
-        transform.position = new Vector2(0f, -screenSize.y + len);
-        transform.rotation = Quaternion.Euler(Vector3.zero);
-        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + Mathf.Rad2Deg * (-Mathf.PI / 2));
-        saveTransform();
-        render();
-    }
-
-    private void render()
-    {
-        for (int i = 0; i < todo.Length; i++)
+        // Assiging values to our new state's values while instantiating
+        state = new Chapter8Fig10LSystemState()
         {
-            char[] todoCharArray = todo.ToCharArray();
-            char c = todoCharArray[i];
+            X = 0,
+            Y = 0,
+            Size = len,
+            Angle = 0
+        };
+
+        string sentence = lSys.Sentence;
+        for(int i = 0; i < sentence.Length; i++)
+        {
+            char c = sentence[i];
             if (c == 'F' || c == 'G')
             {
-                GameObject g = new GameObject();
-                g.transform.position = currentTransform.Position;
-                g.transform.rotation = currentTransform.Rotation;
-                LineRenderer line = g.AddComponent<LineRenderer>();
-                line.positionCount = 2;
-                line.useWorldSpace = true;
-                line.SetPosition(0, Vector2.zero);
-                line.SetPosition(1, new Vector2(len, 0));
-                currentTransform.Position += new Vector3(len, 0);
-                line.startWidth = 0.1f;
-                line.endWidth = 0.1f;
-                
+                line();
             }
             else if (c == '+')
             {
-                currentTransform.Rotation = Quaternion.Euler(currentTransform.Rotation.x, currentTransform.Rotation.y, currentTransform.Rotation.z + theta);
-                
+                state.Angle += theta;
             }
             else if (c == '-')
             {
-                currentTransform.Rotation = Quaternion.Euler(currentTransform.Rotation.x, currentTransform.Rotation.y, currentTransform.Rotation.z - theta);
-                
+                state.Angle -= theta;
             }
             else if (c == '[')
             {
-                pushTransformStack();
+                savedStates.Push(state.Clone());
             }
             else if (c == ']')
             {
-                currentTransform = popTransformStack();
+                state = savedStates.Pop();
             }
         }
+    }
+
+    private void line()
+    {
+        var lineGO = new GameObject();
+        lineGO.transform.position = Vector3.zero;
+        lineGO.transform.parent = transform;
+
+        lines.Add(lineGO);
+        LineRenderer newLine = setupLine(lineGO);
+        newLine.SetPosition(0, new Vector3(state.X + transform.position.x, state.Y + transform.position.y, transform.position.z));
+        checkAngles();
+        newLine.SetPosition(1, new Vector3(state.X + transform.position.x, state.Y + transform.position.y, transform.position.z));
+    }
+
+    private void checkAngles()
+    {
+        if (state.Angle != 0)
+        {
+            state.X += Mathf.Sin(state.Angle / 100);
+            state.Y += Mathf.Cos(state.Angle / 100);
+        }
+        else
+        {
+            state.Y += state.Size;
+        }
+    }
+
+    private LineRenderer setupLine(GameObject lineGO)
+    {
+        var newLine = lineGO.AddComponent<LineRenderer>();
+        newLine.useWorldSpace = true;
+        newLine.positionCount = 2;
+        newLine.material = lineRenderer.material;
+        newLine.startColor = lineRenderer.startColor;
+        newLine.endColor = lineRenderer.endColor;
+        newLine.startWidth = lineRenderer.startWidth;
+        newLine.endWidth = lineRenderer.endWidth;
+        return newLine;
     }
 
     // Update is called once per frame
@@ -117,17 +146,39 @@ public class Chapter8Fig10 : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (counter < 5)
+            if (counter < 4)
             {
-                pushTransformStack();
                 lSys.Generate();
-                todo = lSys.Sentence;
                 len *= 0.5f;
-                currentTransform = popTransformStack();
-                redraw();
+                drawLines();
                 counter++;
             }
         }
+    }
+
+    private void setupCamera()
+    {
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 30;
+        // Get the size of the screen in meters or Unity units
+        screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+}
+
+// Our state of the current L system.
+public class Chapter8Fig10LSystemState
+{
+    public float Size;
+    public float Angle;
+    public float X;
+    public float Y;
+
+    public Chapter8Fig10LSystemState Clone()
+    {
+        // Memeberwise clone performs a shallow copy of an object and returns it. The nonstatic fields get transferred over to the new object.
+        // This returns a base object which we can cast into a system state object
+        // Documentation: https://docs.microsoft.com/en-us/dotnet/api/system.object.memberwiseclone?view=netcore-3.1
+        return (Chapter8Fig10LSystemState)MemberwiseClone();
     }
 }
 
