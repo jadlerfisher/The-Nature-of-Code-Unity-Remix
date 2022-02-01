ea101f3e2b0f8b01f28f385b981f84a5677895e1
mcdonaldduncan <380939@colum.edu>
2022-02-01 11:31:31 -0600

    Refactor Example I.4
    Very minor changes on this one
    
    remove unnecessary collider manipulation
    
    remove copy/pasted notes that do not apply and replaced with accurate notes

--- Assets/Introduction/Example i.4/IntroductionFig4.cs
+++ Assets/Introduction/Example i.4/IntroductionFig4.cs
@@ -5,25 +5,23 @@ using UnityEngine;
 public class IntroductionFig4 : MonoBehaviour
 {
     public Material transparencyPrefab;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        //To create a Gaussian distribution in Unity we can actually use Random.Range() on two separate Random.Ranges
+        //To create a Gaussian distribution in Unity we can use Random.Range() within two separate Random.Range()
         float num = Random.Range(Random.Range(-10f, 10f), Random.Range(-10f, 10f));
         float sd = 30;
         float mean = 5;
 
         float x = sd * num + mean;
 
-        //This creates a sphere GameObject
+        //This creates a sphere GameObject and applies the transparency material prefab we created in Unity.
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         Renderer r = sphere.GetComponent<Renderer>();
-        sphere.GetComponent<SphereCollider>().enabled = false;
-        Object.Destroy(sphere.GetComponent<SphereCollider>());
         r.material = transparencyPrefab;
 
-        //This sets our ink "sphere game objects" at the position of the Walker GameObject (walkerGO) at the current frame
-        //to draw the path
+        //This instantiates the sphere with an X position calculated by our Gaussian distribution
         sphere.transform.position = new Vector3(x, 0F, 0F) * Time.deltaTime;
     }
     
