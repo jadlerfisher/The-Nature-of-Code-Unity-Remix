aac5d891831a4f92b909699f5044cdc7b2942188
mcdonaldduncan <380939@colum.edu>
2022-02-01 10:12:05 -0600

    Refactor Example I.2
    Rename rando to rand
    Put webGL note inline
    Change the arbitrarily assigned width value to 3 from 4
    	- 4 left one section without a cube, 3 works better
    Rewrite notes in update to properly reflect what the code is doing
    
    "Fake" screen height is assigned to zero and never changed, then "added" to the y scale determined by index. Value never changes any functionality and does not affect outcome so it was removed entirely.
    
    Collider is both disabled and destroyed despite the collider having no impact on the functionality of the example. Removed all lines relating to removal of collider.
    
    Following lines of code scaled the objects on instantiation prior to random scaling, scale was determined by the int at index x so this made the skew of the scale even greater than it is already with the small sample size. Notes also did not describe this action correctly until I changed them, at which point I realized what was actually happening.
    //The cube is scaled along the Y axis by taking the randomly assigned int at index x and passing it in to the Y value of a new Vector3. //cube.transform.localScale = new Vector3(1f, (randomCount[x]), 1f);
    
    Without a parent object, this line of code does nothing so was removed
     //cube.transform.localPosition += new Vector3(0f, rand, 0f) / 100 * Time.deltaTime;
    
    Slowed scaling by a factor of 10 to allow for longer viewing time
    
    moved camera down along y axis to account for cubes slower scaling and no longer scaling prematurely
    
    This example could use a rebuild

--- Assets/Introduction/Example i.2/IntroductionFig2.cs
+++ Assets/Introduction/Example i.2/IntroductionFig2.cs
@@ -7,16 +7,14 @@ public class IntroductionFig2 : MonoBehaviour
    List<int> randomCount = new List<int>();
    List<GameObject> cubes = new List<GameObject>();
 
-    // Set a fake creeen height for Unity
-   int height = 0;
-
     // Start is called before the first frame update
     void Start()
     {
         while(randomCount.Count < 40)
         {
-            //Grab a random number betwen -1 and the number of values in our random number list
-            int index = Random.Range(-1, 40);
+            //Grab a random number betwen -1 and 40
+            //We use 41 instead of 40 because Random.Range is max exclusive while using integer values
+            int index = Random.Range(-1, 41);
             //if the number does not exist in the list, add it
             if (!randomCount.Contains(index))
             {
@@ -29,32 +27,28 @@ public class IntroductionFig2 : MonoBehaviour
     void FixedUpdate()
     {
         //Create a fake width to maintain our cubes in a reasonable 3D space
-        int w = 4;
+        int w = 3;
         if (cubes.Count < randomCount.Count)
         {
-            //For each of the random values, up until 100 (for memory) create a cube and place it at a new Vector
+            //For each of the random values, up until 40 (for memory) create a cube and place it at a new Vector
             for (int x = 0; x < randomCount.Count; x++)
             {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);     //We need to create a new material for WebGL
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);     
+                //We need to create a new material for WebGL
                 Renderer r = cube.GetComponent<Renderer>();
-                cube.GetComponent<Collider>().enabled = false;
-                Object.Destroy(gameObject.GetComponent<Collider>());
                 r.material = new Material(Shader.Find("Diffuse"));
-                //This new vector is created by multiplying the cube's index (an int) and multiplying it by the fake width of the screen. 
-                //The Y coordinate is chosen by subtracting the cube's randomly chosen int and subtracting it from the height. In this case 0.
-                cube.transform.position = new Vector3(w - x, 0f, 0F);
-                cube.transform.localScale = new Vector3(1f, (height + randomCount[x]), 1F);
+                //The X coordinate is chosen by subtracting the cube's index x from the width w. In this case 3.
+                cube.transform.position = new Vector3(w - x, 0f, 0f);
                 cubes.Add(cube);
             }
         }
 
-            foreach(GameObject cube in cubes)
-            {
-                float rando = randomCount[Random.Range(0, randomCount.Count)];
-                //Now for each of the cubes, we will scale the Y value, its height, by choosing a random value from the list randomCount
-                cube.transform.localScale += new Vector3(0f, rando, 0f) * Time.deltaTime;
-                cube.transform.localPosition += (new Vector3(0f, rando, 0f)/100 * Time.deltaTime);
-            }
+        foreach (GameObject cube in cubes)
+        {
+            int rand = randomCount[Random.Range(0, randomCount.Count)];
+            //Now for each of the cubes, we will scale the Y value, its height, by choosing a random value from the list randomCount
+            cube.transform.localScale += new Vector3(0f, rand, 0f) / 10 * Time.deltaTime;
+        }
     }
 }
 
