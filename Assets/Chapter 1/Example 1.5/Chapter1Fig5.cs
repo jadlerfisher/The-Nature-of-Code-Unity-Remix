9fe23de389292a0cc769d187f443b06728dcc889
mcdonaldduncan <380939@colum.edu>
2022-02-08 12:29:14 -0600

    Update Chapter 1 Fig 5
    changed style conventions and names to match previous examples and standard Unity conventions

--- Assets/Chapter 1/Example 1.5/Chapter1Fig5.cs
+++ Assets/Chapter 1/Example 1.5/Chapter1Fig5.cs
@@ -6,10 +6,11 @@
 
 public class Chapter1Fig5 : MonoBehaviour
 {
-    // These objects are brought in from the unity scene
-    public Camera camera;
-    public GameObject centerSphere;
-    public GameObject cursorSphere;
+    // These objects are brought in from the Unity scene and must be assigned in the inspector
+    // [SerializeField] is used to make private variables visible in the Unity inspector
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject centerSphere;
+    [SerializeField] private GameObject cursorSphere;
 
     // Create variables for rendering the line between two vectors
     private LineRenderer lineRender;
@@ -24,9 +25,9 @@ public class Chapter1Fig5 : MonoBehaviour
         GameObject newB = new GameObject();
         lineRender = newA.AddComponent<LineRenderer>();
         magLineRender = newB.AddComponent<LineRenderer>();
-        //We need to create a new material for WebGL
+        // We need to create a new material for WebGL
         lineRender.material = new Material(Shader.Find("Diffuse"));
-        //We need to create a new material for WebGL
+        // We need to create a new material for WebGL
         magLineRender.material = new Material(Shader.Find("Diffuse"));
     }
 
@@ -34,36 +35,36 @@ public class Chapter1Fig5 : MonoBehaviour
     void Update()
     {
         // Track the Vector2 of the mouse's position and the center sphere's position
-        Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 centerPos = centerSphere.transform.position;
         // Move the cursor sphere directly to the mouse
         cursorSphere.transform.position = mousePos;
 
         // Calculate the magnitude(the distance between the two spheres)
-        Vector2 differenceVector = subtractVectors(mousePos, centerPos);
-        float magnitude = magnitudeOf(differenceVector);
+        Vector2 differenceVector = SubtractVectors(mousePos, centerPos);
+        float magnitude = MagnitudeOf(differenceVector);
 
         // Render the line between the spheres directly 
         lineRender.SetPosition(0, centerPos);
         lineRender.SetPosition(1, mousePos);
 
         // Render a bar in the top left of the screen with the same length as the vector
-        Vector2 cameraTopLeft = camera.ScreenToWorldPoint(new Vector2(0, Screen.height));
+        Vector2 cameraTopLeft = cam.ScreenToWorldPoint(new Vector2(0, Screen.height));
         magLineRender.SetPosition(0, cameraTopLeft);
-        // Use of Unity's built in Vector2 operators:
-        magLineRender.SetPosition(1, cameraTopLeft + magnitude * Vector2.right);
+        // Use of Unity's built in Vector2 operators, Vector2.right and scale by magnitude
+        magLineRender.SetPosition(1, cameraTopLeft + Vector2.right * magnitude);
     }
 
     // This method finds the length of a vector using pythagoras theorem
     // magnitudeOf(vec) will yield the same output as Unity's built in property vect.magnitude
-    float magnitudeOf(Vector2 vector)
+    float MagnitudeOf(Vector2 vector)
     {
         return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y);
     }
 
     // This method calculates A - B component wise
     // subtractVectors(vecA, vecB) will yield the same output as Unity's built in operator: vecA - vecB
-    Vector2 subtractVectors(Vector2 vectorA, Vector2 vectorB)
+    Vector2 SubtractVectors(Vector2 vectorA, Vector2 vectorB)
     {
         float newX = vectorA.x - vectorB.x;
         float newY = vectorA.y - vectorB.y;
