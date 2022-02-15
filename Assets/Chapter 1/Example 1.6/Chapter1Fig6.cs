be3504efd258687c0df342710765f0303e9e4001
mcdonaldduncan <380939@colum.edu>
2022-02-15 08:43:08 -0600

    Update Chapter 1 Fig 6
    Change names and notes to match previous changes and Unity conventions
    SerializeField
    Capitalize Methods
    Add more accurate notes

--- Assets/Chapter 1/Example 1.6/Chapter1Fig6.cs
+++ Assets/Chapter 1/Example 1.6/Chapter1Fig6.cs
@@ -6,10 +6,11 @@
 
 public class Chapter1Fig6 : MonoBehaviour
 {
-    // These objects are brought in from the unity scene
-    public Camera camera;
-    public GameObject centerSphere;
-    public GameObject cursorSphere;
+    // These objects are brought in from the Unity scene and must be assigned in the inspector
+    // [SerializeField] is used to make private variables visible in the Unity inspector
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject centerSphere;
+    [SerializeField] private GameObject cursorSphere;
 
     // Create variables for rendering the line between two vectors
     private LineRenderer lineRender;
@@ -19,7 +20,7 @@ public class Chapter1Fig6 : MonoBehaviour
     {
         // Create the line renderer component on this script's GameObject
         lineRender = gameObject.AddComponent<LineRenderer>();
-        //We need to create a new material for WebGL
+        // We need to create a new material for WebGL
         lineRender.material = new Material(Shader.Find("Diffuse"));
     }
 
@@ -27,15 +28,18 @@ public class Chapter1Fig6 : MonoBehaviour
     void Update()
     {
         // Track the Vector2 of the mouse's position and the center sphere's position
-        Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 centerPos = centerSphere.transform.position;
 
         // Calculate the magnitude(the distance between the two spheres)
-        Vector2 differenceVector = subtractVectors(mousePos, centerPos);
-        // Resize the vector to be the length one meter
-        Vector2 unitVector = normalizedOf(differenceVector);
+        Vector2 differenceVector = SubtractVectors(mousePos, centerPos);
 
+        // Resize the vector to be the length one meter
+        Vector2 unitVector = NormalizedOf(differenceVector);
+
+        // Set the position of the cursor sphere to the location of the normalized vector
         cursorSphere.transform.position = unitVector;
+
         // Render the line between the spheres directly 
         lineRender.SetPosition(0, centerPos);
         lineRender.SetPosition(1, unitVector);
@@ -43,36 +47,36 @@ public class Chapter1Fig6 : MonoBehaviour
 
     // This method scales the length(magnitude) of a vector to be 1
     // normalizedOf(vec) will yield the same output as Unity's built in vec.normalized
-    Vector2 normalizedOf(Vector2 vector)
+    Vector2 NormalizedOf(Vector2 vector)
     {
         // To get the length of the vector to one we have multiply by the reciprocal:
         //               1
         //   length * -------- = 1
         //             length
-        float length = magnitudeOf(vector);
-        Vector2 lengthOneVector = multiplyVector(vector, 1 / length);
+        float length = MagnitudeOf(vector);
+        Vector2 lengthOneVector = ScaleVector(vector, 1 / length);
         return lengthOneVector;
     }
 
-    // This method finds the length of a vector using pythagoras theorem
+    // This method finds the length of a vector using the pythagorean theorem
     // magnitudeOf(vec) will yield the same output as Unity's built in property vect.magnitude
-    float magnitudeOf(Vector2 vector)
+    float MagnitudeOf(Vector2 vector)
     {
         return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y);
     }
 
     // This method calculates A - B component wise
     // subtractVectors(vecA, vecB) will yield the same output as Unity's built in operator: vecA - vecB
-    Vector2 subtractVectors(Vector2 vectorA, Vector2 vectorB)
+    Vector2 SubtractVectors(Vector2 vectorA, Vector2 vectorB)
     {
         float newX = vectorA.x - vectorB.x;
         float newY = vectorA.y - vectorB.y;
         return new Vector2(newX, newY);
     }
 
-    // This method calculates A * b component wise
-    // multiplyVector(vector, factor) will yield the same output as Unity's built in operator: vector * factor
-    Vector2 multiplyVector(Vector2 toMultiply, float scaleFactor)
+    // This method calculates a vector scaled by a factor component wise
+    // ScaleVector(vector, factor) will yield the same output as Unity's built in operator: vector * factor
+    Vector2 ScaleVector(Vector2 toMultiply, float scaleFactor)
     {
         float x = toMultiply.x * scaleFactor;
         float y = toMultiply.y * scaleFactor;
