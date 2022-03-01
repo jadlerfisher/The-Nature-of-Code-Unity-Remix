c3098ea9a85be2109d313bb37a6a031c494d2470
mcdonaldduncan <380939@colum.edu>
2022-03-01 08:51:35 -0600

    Update Chapter2Fig1.cs
    Add functionality to stop spheres more reliably at walls
    rename checkboundaries to match previous chapters

--- Assets/Chapter 2/Example 2.1/Chapter2Fig1.cs
+++ Assets/Chapter 2/Example 2.1/Chapter2Fig1.cs
@@ -28,7 +28,7 @@ public class Chapter2Fig1 : MonoBehaviour
         // Apply force to the mover, with ForceMode.Impulse taking mass into account
         mover.body.AddForce(wind, ForceMode.Impulse);
 
-        mover.CheckBoundaries();
+        mover.CheckEdges();
     }
 }
 
@@ -54,6 +54,7 @@ public class Mover2_1
 
         // Remove functionality that come with the primitive that we don't want
         // Destroy() is a built in Unity function that removes a GameObject
+        gameObject.GetComponent<SphereCollider>().enabled = false;
         Object.Destroy(gameObject.GetComponent<SphereCollider>());
 
         // Generate a radius of 1f for this mover
@@ -74,10 +75,9 @@ public class Mover2_1
     }
 
     // Checks to ensure the body stays within the boundaries
-    public void CheckBoundaries()
+    public void CheckEdges()
     {
         Vector3 restrainedVelocity = body.velocity;
-        // We add or subtract the radius to ensure the sphere bounces off the boundary instead of sinking halfway through
         if (body.position.y - radius < yMin)
         {
             // Using the absolute value here is an important safe
@@ -86,15 +86,19 @@ public class Mover2_1
             // The intuitive solution of flipping the velocity may result
             // in the mover not returning to the boundaries and flipping
             // direction on every tick.
+
             restrainedVelocity.y = Mathf.Abs(restrainedVelocity.y);
+            body.position = new Vector3(body.position.x, yMin, body.position.z) + Vector3.up * radius;
         }
         if (body.position.x - radius < xMin)
         {
             restrainedVelocity.x = Mathf.Abs(restrainedVelocity.x);
+            body.position = new Vector3(xMin, body.position.y, body.position.z) + Vector3.right * radius;
         }
         else if (body.position.x + radius > xMax)
         {
             restrainedVelocity.x = -Mathf.Abs(restrainedVelocity.x);
+            body.position = new Vector3(xMax, body.position.y, body.position.z) + Vector3.left * radius;
         }
         body.velocity = restrainedVelocity;
     }
