e26976bb521b94c4c8f86ab62b9d1a109fcc6ee7
mcdonaldduncan <380939@colum.edu>
2022-03-08 11:13:02 -0600

    Update Chapter3Fig5.cs
    Minor formatting adjustments, added a few comment clarifications
    
    simplify repeat getcomponent calls

--- Assets/Chapter 3/Example 3.5/Chapter3Fig5.cs
+++ Assets/Chapter 3/Example 3.5/Chapter3Fig5.cs
@@ -10,7 +10,6 @@ public class Chapter3Fig5 : MonoBehaviour
     private GameObject sphere;
     private MeshRenderer sphereRenderer;
 
-
     //Create variables for rendering the line between two vectors
     private GameObject lineDrawing;
     private LineRenderer lineRender;
@@ -18,22 +17,29 @@ public class Chapter3Fig5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Set the camera to orthographic with a size of 5
         Camera.main.orthographic = true;
         Camera.main.orthographicSize = 5;
-        // Create a GameObject that will be the line
+
+        // Create a GameObject that will hold the LineRenderer component
         lineDrawing = new GameObject();
 
+        // Create a sphere to oscillate
         sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
         //We need to create a new material for WebGL
         sphereRenderer = sphere.GetComponent<MeshRenderer>();
         sphereRenderer.material = new Material(Shader.Find("Diffuse"));
 
-        //Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a bright pink line.
+        //Add the Unity Component "LineRenderer" to the GameObject lineDrawing. We will see a black line.
         lineRender = lineDrawing.AddComponent<LineRenderer>();
+
+        //Make the line smaller for aesthetics
+        lineRender.startWidth = 0.1f;
+        lineRender.endWidth = 0.1f;
+
         //We need to create a new material for WebGL
         lineRender.material = new Material(Shader.Find("Diffuse"));
-        lineRender.GetComponent<LineRenderer>().startWidth = 0.1f;
-        lineRender.GetComponent<LineRenderer>().endWidth = 0.1f;
     }
 
     // Update is called once per frame
@@ -41,7 +47,8 @@ public class Chapter3Fig5 : MonoBehaviour
     {
         float x = amplitude * Mathf.Cos((2*Mathf.PI)* Time.time/period);
         sphere.transform.position = new Vector2(x, 0f);
-        //Begin rendering the line between the two objects. Set the first point (0) at the centerSphere Position
+
+        //Begin rendering the line between the two objects. Set the first point (0) at the center Position
         //Make sure the end of the line (1) appears at the new Vector3
         Vector2 center = Vector2.zero;
         lineRender.SetPosition(0, center);
