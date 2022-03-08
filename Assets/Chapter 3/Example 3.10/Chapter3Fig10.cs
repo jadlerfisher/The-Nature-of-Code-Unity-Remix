9c463518d201f14a9259668c291fc84f49c776fa
mcdonaldduncan <380939@colum.edu>
2022-03-08 12:12:41 -0600

    Update Chapter3Fig10.cs
    serialize fields
    
    change position placement to use findwindowlimits()
    
    clarify comments

--- Assets/Chapter 3/Example 3.10/Chapter3Fig10.cs
+++ Assets/Chapter 3/Example 3.10/Chapter3Fig10.cs
@@ -5,26 +5,41 @@ using UnityEngine;
 public class Chapter3Fig10 : MonoBehaviour
 {
     // Get the gravity strength and the pendulum from the scene
-    public float gravity;
-    public float damping;
-    public float radius;
-    public float startingAngleDegrees;
+    [SerializeField] float gravity;
+    [SerializeField] float damping;
+    [SerializeField] float radius;
+    [SerializeField] float startingAngleDegrees;
 
     Pendulum pendulum;
 
+    Vector2 maximumPos;
+
     void Start()
     {
+        FindWindowLimits();
+
         // Create a new instance of the Pendulum behavior
         pendulum = gameObject.AddComponent<Pendulum>();
+
         // Position the pivot for the pendulum at the top center of the camera
-        pendulum.pivot = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width / 2, Screen.height));
-        // Pass the values into the new pendulum
+        pendulum.pivot = new Vector2(0, maximumPos.y);
+
+        // Pass the values we assigned into the new pendulum
         pendulum.gravity = gravity;
         pendulum.damping = damping;
         pendulum.radius = radius;
+
         // Adjust the angle since 0 degrees should point down, not up
         pendulum.angle = (180 - startingAngleDegrees) * Mathf.Deg2Rad;
+    }
 
-
+    private void FindWindowLimits()
+    {
+        // Start by setting the camera's projection to Orthographic mode
+        Camera.main.orthographic = true;
+        // For correct functionality, we set the camera x and y position to 0, 0
+        Camera.main.transform.position = new Vector3(0, 0, -10);
+        // Next we grab the minimum and maximum position for the screen
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 }
