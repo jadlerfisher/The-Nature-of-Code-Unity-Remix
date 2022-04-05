a88b7b9ae35cb92a67e9d0382cae29ef4a6c33ff
mcdonaldduncan <380939@colum.edu>
2022-04-05 03:58:55 -0500

    Update Ch6 Fig6

--- Assets/Chapter 6/Example 6.6/Example6_6.cs
+++ Assets/Chapter 6/Example 6.6/Example6_6.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class Example6_6 : MonoBehaviour
 {
     // Get scene references:
-    public GameObject vehiclePrefab;
-    public Path6_6 path;
+    [SerializeField] GameObject vehiclePrefab;
+    [SerializeField] Path6_6 path;
     // Get example parameters:
-    public int vehicleCount;
-    public float minSpeed, maxSpeed;
+    [SerializeField] int vehicleCount;
+    [SerializeField] float minSpeed, maxSpeed;
 
     private Vehicle6_6[] vehicles;
-    private float screenLeft, screenRight;
+    private Vector3 maximumPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get the left and right screen bounds for screen-wrapping logic.
-        screenLeft = Camera.main.ScreenToWorldPoint(Vector2.zero).x;
-        screenRight = Camera.main.ScreenToWorldPoint(Vector2.right * Screen.width).x;
+        FindWindowLimits();
 
         // Spawn the vehicles into the scene.
         vehicles = new Vehicle6_6[vehicleCount];
@@ -41,10 +39,10 @@ public class Example6_6 : MonoBehaviour
         foreach (Vehicle6_6 vehicle in vehicles)
         {
             // When the vehicle passes the right edge, wrap back to the left edge.
-            if (vehicle.transform.position.x > screenRight)
+            if (vehicle.transform.position.x > maximumPos.x)
             {
                 vehicle.transform.position = new Vector3(
-                    screenLeft,
+                    -maximumPos.x,
                     vehicle.transform.position.y,
                     vehicle.transform.position.z
                 );
@@ -52,5 +50,17 @@ public class Example6_6 : MonoBehaviour
             // Attempt to follow the path.
             vehicle.FollowPath(path);
         }
+    }
+
+    private void FindWindowLimits()
+    {
+        // We want to start by setting the camera's projection to Orthographic mode
+        Camera.main.orthographic = true;
+
+        // For FindWindowLimits() to function correctly, the camera must be set to coordinates 0, 0, -10
+        Camera.main.transform.position = new Vector3(0, 0, -5);
+
+        // Next we grab the maximum position for the screen
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 }
