24b7ae5ad922d56da9568e90f34236bf3be45176
mcdonaldduncan <380939@colum.edu>
2022-04-19 13:43:47 -0500

    Update Chapter10Fig3.cs

--- Assets/Chapter 10/Example 10.3/Chapter10Fig3.cs
+++ Assets/Chapter 10/Example 10.3/Chapter10Fig3.cs
@@ -6,27 +6,28 @@ public class Chapter10Fig3 : MonoBehaviour
 {
     Vector2 maximumPos;
     Network network;
+
     // Start is called before the first frame update
     void Start()
     {
-        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        FindWindowLimits();
 
-        //Create the Network Object
+        // Create the Network Object
         network = new Network(maximumPos.x / 2, maximumPos.y / 2);
 
-        //Create a bunch of neurons
+        // Create a bunch of neurons
         Neuron a = new Neuron(-20, 0);
         Neuron b = new Neuron(0, 7);
         Neuron c = new Neuron(0, -7);
         Neuron d = new Neuron(20, 0);
 
-        //Connect them
+        // Connect them
         network.connect(a, b);
         network.connect(a, c);
         network.connect(b, d);
         network.connect(c, d);
 
-        //Add them to the network
+        // Add them to the network
         network.addNeuron(a);
         network.addNeuron(b);
         network.addNeuron(c);
@@ -40,15 +41,26 @@ public class Chapter10Fig3 : MonoBehaviour
         network.display();
     }
 
-    
+    private void FindWindowLimits()
+    {
+        // We want to start by setting the camera's projection to Orthographic mode
+        Camera.main.orthographic = true;
+
+        // For FindWindowLimits() to function correctly, the camera must be set to coordinates 0, 0, -10
+        Camera.main.transform.position = new Vector3(0, 0, -10);
+
+        // Next we grab the maximum position for the screen
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+
 }
 
 public class Neuron
 {
-    //Neuron has a position
+    // Neuron has a position
     public Vector2 position;
 
-    //Neuron has a list of connections
+    // Neuron has a list of connections
     List<Connection> connections = new List<Connection>();
 
     public Neuron(float x, float y)
@@ -56,13 +68,13 @@ public class Neuron
         position = new Vector2(x, y);
     }
 
-    //Add a connection
+    // Add a connection
     public void addConnection(Connection c)
     {
         connections.Add(c);
     }
 
-    //Draw the Neuron as a circle
+    // Draw the Neuron as a circle
     public void display()
     {
         GameObject neuronGO = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -70,7 +82,7 @@ public class Neuron
         r.material = new Material(Shader.Find("Diffuse"));
         neuronGO.transform.position = position;
 
-        //Draw all of its connections
+        // Draw all of its connections
         foreach (Connection c in connections)
         {
             c.display();
@@ -80,11 +92,11 @@ public class Neuron
 
 public class Connection
 {
-    //Connection from Neuron A to B
+    // Connection from Neuron A to B
     Neuron a;
     Neuron b;
 
-    //Connection has a weight
+    // Connection has a weight
     float weight;
 
     GameObject theLine;
@@ -97,7 +109,7 @@ public class Connection
         b = to;
     }
 
-    //Draw as a Line
+    // Draw as a Line
     public void display()
     {
         theLine = new GameObject();
@@ -111,7 +123,7 @@ public class Connection
 
 public class Network
 {
-    //The Network has a list of neurons
+    // The Network has a list of neurons
     List<Neuron> neurons;
     Vector2 position;
 
@@ -121,13 +133,13 @@ public class Network
         neurons = new List<Neuron>();
     }
 
-    //We can add a Neuron
+    // We can add a Neuron
     public void addNeuron(Neuron n)
     {
         neurons.Add(n);
     }
 
-    //We can connect the two Neurons
+    // We can connect the two Neurons
     public void connect(Neuron a, Neuron b)
     {
         Connection c = new Connection(a, b, Random.Range(0f, 1f));
