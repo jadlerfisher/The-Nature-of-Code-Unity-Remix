ba8889cbf95b01dfe2b4c55c04a57fd2bccec97d
mcdonaldduncan <380939@colum.edu>
2022-04-26 10:32:44 -0500

    Edits for consistency
    restructure comments to display better in book
    restructure window limits

--- Assets/Chapter 8/Example 8.4/Chapter8Fig4.cs
+++ Assets/Chapter 8/Example 8.4/Chapter8Fig4.cs
@@ -4,50 +4,63 @@ using UnityEngine;
 
 public class Chapter8Fig4 : MonoBehaviour
 {
-    private Vector2 minPos, maxPos;
-
+    private Vector2 maximumPos;
 
     void Start()
     {
-        findWindowLimits();
-        cantor(minPos.x + 3, maxPos.y - 2, (maxPos.x - minPos.x) - 6);
+        FindWindowLimits();
+        cantor(-maximumPos.x + 3, maximumPos.y - 2, (maximumPos.x - -maximumPos.x) - 6);
     }
 
     void cantor(float x, float y, float len)
     {
-        line(x, y, x + len, y); // Create the first line
+        // Create the first line
+        line(x, y, x + len, y);
 
-        if (len >= 0.01f) // Stop at 0.01 meters length!
+        // Stop at 0.01 meters length!
+        if (len >= 0.01f) 
         {
             y -= 2;
 
-            cantor(x, y, len / 3); // Call the function recursively 
-            cantor(x + len * 2 / 3, y, len / 3); // We need two lines for each line above it
+            // Call the function recursively 
+            cantor(x, y, len / 3);
+            // We need two lines for each line above it
+            cantor(x + len * 2 / 3, y, len / 3); 
         }
     }
 
     private GameObject line(float x1, float y1, float x2, float y2) 
     {
-        GameObject obj = new GameObject(); // Creates an object to attach a Line Renderer to
-        obj.name = "Line"; // We change the in-scene name so it's easier to identify
-        LineRenderer line = obj.AddComponent<LineRenderer>(); // We attach the LineRenderer to the object and store it as 'line'
+        // Creates an object to attach a Line Renderer to
+        GameObject obj = new GameObject();
+        // We change the in-scene name so it's easier to identify
+        obj.name = "Line";
+        // We attach the LineRenderer to the object and store it as 'line'
+        LineRenderer line = obj.AddComponent<LineRenderer>();
 
-        line.material = new Material(Shader.Find("Sprites/Default")); // Set the material
-        line.startColor = line.endColor = Color.black; // Set the color
+        // Set the material
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        // Set the color
+        line.startColor = line.endColor = Color.black;
+        // Set the width
+        line.startWidth = line.endWidth = 1f;
 
-        line.startWidth = line.endWidth = 1f; // Set the width
-
-        line.SetPosition(0, new Vector3(x1, y1, 0)); // Set the begining and end points of the line
+        // Set the begining and end points of the line
+        line.SetPosition(0, new Vector3(x1, y1, 0)); 
         line.SetPosition(1, new Vector3(x2, y2, 0));
 
         return obj;
     }
 
-    private void findWindowLimits()
+    private void FindWindowLimits()
     {
+        // We want to start by setting the camera's projection to Orthographic mode
         Camera.main.orthographic = true;
-        Camera.main.orthographicSize = 10;
-        minPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
-        maxPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        // For FindWindowLimits() to function correctly, the camera must be set to coordinates 0, 0, -10
+        Camera.main.transform.position = new Vector3(0, 0, -5);
+
+        // Next we grab the maximum position for the screen
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 }
