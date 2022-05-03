0712005a78c85d2e9db552c48497e4bbf642e95b
mcdonaldduncan <380939@colum.edu>
2022-05-03 12:06:17 -0500

    Revert "update 9.5"
    This reverts commit 0d70a4f6f34ded9188386770d62d703a17d1da7b.

--- Assets/Chapter 9/Example 9.5/Chapter9Fig5.cs
+++ Assets/Chapter 9/Example 9.5/Chapter9Fig5.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Chapter9Fig5 : MonoBehaviour
 {
-    [SerializeField] int num = 10; // Number of Bloops
-    [SerializeField] float foodSpawnRate = 1; // The rate (in seconds) which food spawns
-    private Vector2 maximumPos;
+    public int num = 10; // Number of Bloops
+    public float foodSpawnRate = 1; // The rate (in seconds) which food spawns
+    private Vector2 minPos, maxPos;
 
     void Start()
     {
-        FindWindowLimits();
+        findWindowLimits();
 
         for (int i = 0; i < num; i++) 
         {
@@ -20,7 +20,7 @@ public class Chapter9Fig5 : MonoBehaviour
 
         for (int i = 0; i < num*3; i++) 
         {
-            SpawnFood(); // We'll package food spawning into a function so we can reuse the code!
+            spawnFood(); // We'll package food spawning into a function so we can reuse the code!
         }
 
         StartCoroutine(FoodSpawner());
@@ -31,11 +31,11 @@ public class Chapter9Fig5 : MonoBehaviour
         while (true) 
         {
             yield return new WaitForSeconds(foodSpawnRate);
-            SpawnFood();
+            spawnFood();
         }
     }
 
-    private void SpawnFood() 
+    private void spawnFood() 
     {
         GameObject fd = GameObject.CreatePrimitive(PrimitiveType.Cube);
         BoxCollider bx = fd.AddComponent<BoxCollider>();
@@ -43,23 +43,17 @@ public class Chapter9Fig5 : MonoBehaviour
         r.material = new Material(Shader.Find("Diffuse"));
         r.material.color = Color.grey;
         fd.transform.localScale *= 0.3f; // Let's make the object small
-        fd.transform.position = new Vector2(Random.Range(-maximumPos.x, maximumPos.x), Random.Range(-maximumPos.y, maximumPos.y)); // Set a random location for the food
+        fd.transform.position = new Vector2(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y)); // Set a random location for the food
         fd.layer = 9; // We'll set the object to it's own layer so we can find it much easier later
         fd.name = "Food"; // We'll name it too!
     }
 
-    private void FindWindowLimits()
+
+    private void findWindowLimits()
     {
-        // We want to start by setting the camera's projection to Orthographic mode
         Camera.main.orthographic = true;
-
-        // Set the desired camera size
-        Camera.main.orthographicSize = 10;
-
-        // Set position of the camera to ensure 0,0 x,y
-        Camera.main.transform.position = new Vector3(0, 0, -10);
-
-        // Next we grab the maximum position for the screen
-        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Camera.main.orthographicSize = 5;
+        minPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        maxPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 }
